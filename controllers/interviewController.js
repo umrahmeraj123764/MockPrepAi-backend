86a2481a5a1f7161f0c24398530f42e1d958de56
Umrah Meraj <umrahmeraj4@gmail.com>
2026-08-27 13:29:54 +0530

    changed models

--- controllers/interviewController.js
+++ controllers/interviewController.js
@@ -4,13 +4,14 @@ import Groq from "groq-sdk";
 
 
 export const startInterview=async (req,res)=>{
-    const groq= new Groq ({apiKey:process.env.GROQ_API_KEY})
+   try{
+     const groq= new Groq ({apiKey:process.env.GROQ_API_KEY})
     const {role,type}=req.body
     
     if(!role||!type) return res.status(400).json({message:"doesnt exist"})
 
         const response =await groq.chat.completions.create({
-            model: "llama-3.1-8b-instant" ,
+            model: "openai/gpt-oss-20b",
         messages: [{role:"user" , content: `Generate 5 ${type} interview question for ${role}.Return only a JSON array with no extra text like:["question1", "question2","question3", "question4","question5"]`}]
 
         })
@@ -22,16 +23,20 @@ export const startInterview=async (req,res)=>{
             questions:parsed
         })
         res.status(201).json(session)
+    } catch (error) {
+        console.error("Groq error:", error);
+        res.status(500).json({ message: "Failed to start interview session" });
+    }
+};
 
 
-}
 
 export const giveResponse=async(req,res)=>{
     const groq= new Groq ({apiKey:process.env.GROQ_API_KEY})
     const {sessionId,question,answer}=req.body
     try{
     const response=await groq.chat.completions.create({
-        model: "llama-3.1-8b-instant",
+        model: "openai/gpt-oss-20b",
         messages:[{
         role:"user",
         content:`question:${question}
